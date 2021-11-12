0842c2f16a94d9352d8ac624f7859244a29d2551
david942j <david942j@gmail.com>
2021-11-12 16:08:26 +0000

    chaos: qemu eventfd-based protocol succeeded

--- src/chaos/entry.cpp
+++ src/chaos/entry.cpp
@@ -4,40 +4,282 @@
  * Copyright (c) 2021 david942j
  */
 
-#include <cstdio>
+#include <sys/eventfd.h>
 #include <sys/mman.h>
-#include <cassert>
-#include <cstdint>
+#include <sys/select.h>
 #include <unistd.h>
 
-#define CSR_FD 3
-#define CSR_BASE 0x10000
-#define DRAM_FD 4
-#define DRAM_BASE 0x10000000
+#include <cassert>
+#include <cerrno>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstring>
+
+#define CHECK assert
+
+namespace {
 
 class MemoryRegion {
  public:
-  MemoryRegion(int fd, uint64_t base) : fd_(fd), base_((void*)base) {
+  MemoryRegion(uint64_t base, uint64_t size, int prot) : base_((void *)base), size_(size) {
+    auto ptr = mmap(base_, size_, prot, MAP_ANONYMOUS | MAP_PRIVATE | MAP_FIXED, -1, 0);
+    CHECK(ptr == base_);
+  }
+
+  MemoryRegion(int fd, uint64_t base) : base_((void *)base) {
     lseek(fd, 0, SEEK_SET);
     size_ = lseek(fd, 0, SEEK_END);
-    auto ptr = mmap(base_, size_, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    assert(ptr == base_);
+    auto ptr = mmap(base_, size_, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_FIXED, fd, 0);
+    CHECK(ptr == base_);
+    close(fd);
   }
+
   ~MemoryRegion() {
     munmap(base_, size_);
   }
 
+  inline uint64_t size() const { return size_; }
+
+  uint64_t Read64(uint64_t offset) const {
+    CHECK(offset <= size_ - 8);
+    return *static_cast<uint64_t *>(base_ + offset);
+  }
+
+  void Write64(uint64_t offset, uint64_t val) const {
+    CHECK(offset <= size_ - 8);
+    *static_cast<uint64_t *>(base_ + offset) = val;
+  }
+
+  void *at(uint64_t offset) const {
+      CHECK(offset < size_);
+      return base_ + offset;
+  }
+
  private:
-  int fd_;
   void *base_;
   uint64_t size_;
 };
 
-static MemoryRegion CSR(CSR_FD, CSR_BASE);
-static MemoryRegion DRAM(DRAM_FD, DRAM_BASE);
+class Event {
+ public:
+  Event(int fd) : fd_(fd) {
+    CHECK(fd < FD_SETSIZE);
+  }
+  ~Event() {
+    close(fd_);
+  }
+
+  void WaitAndClear() const {
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(fd_, &readfds);
+    int ret = select(fd_ + 1, &readfds, NULL, NULL, NULL);
+    CHECK(ret == 1);
+    uint64_t dummy;
+    ret = read(fd_, &dummy, sizeof(dummy));
+    CHECK(ret == sizeof(dummy));
+  }
+
+  void Trigger() const {
+    uint64_t one;
+    int ret = write(fd_, &one, sizeof(one));
+    CHECK(ret == sizeof(one));
+  }
+
+ private:
+  int fd_;
+};
+
+constexpr int kCsrFd = 3;
+constexpr int kDramFd = 4;
+constexpr uint64_t kCsrBase   = 0x00010000;
+constexpr uint64_t kDramBase  = 0x10000000;
+constexpr uint64_t kCodeBase  = 0x00100000;
+constexpr uint64_t kCodeSize  = 0x00100000; // 1MB
+constexpr uint64_t kStackBase = 0x00201000;
+constexpr uint64_t kStackSize = 0x00002000;
+
+MemoryRegion CSR(kCsrFd, kCsrBase);
+MemoryRegion DRAM(kDramFd, kDramBase);
+MemoryRegion Code(kCodeBase, kCodeSize, PROT_READ | PROT_WRITE | PROT_EXEC);
+MemoryRegion Stack(kStackBase, kStackSize, PROT_READ | PROT_WRITE);
+
+} // namespace
+
+// TODO: move to "firmware"
+namespace firmware {
+
+enum chaos_command_code {
+	CHAOS_CMD_CODE_REQUEST,
+};
+
+struct chaos_mailbox_cmd {
+    uint32_t seq;
+    enum chaos_command_code code;
+    uint32_t dma_addr;
+    uint32_t dma_size;
+};
+
+struct chaos_mailbox_rsp {
+    uint32_t seq;
+    uint32_t retval;
+};
+
+enum chaos_request_algo {
+	/* copy input to output, for testing purpose */
+	CHAOS_ALGO_ECHO,
+	CHAOS_ALGO_MD5,
+};
+
+struct chaos_request {
+	enum chaos_request_algo algo;
+	uint32_t input;
+	uint32_t in_size;
+	uint32_t output;
+	uint32_t out_size;
+};
+
+struct Csrs {
+    uint64_t version; /* R */
+    uint64_t cmdq_addr; /* RW */
+    uint64_t rspq_addr; /* RW */
+    uint64_t cmdq_size; /* RW */
+    uint64_t rspq_size; /* RW */
+    uint64_t reset; /* W */
+    uint64_t irq_status; /* R */
+    uint64_t clear_irq; /* W */
+    uint64_t cmd_sent; /* W */
+    uint64_t cmd_head; /* R */
+    uint64_t cmd_tail; /* W */
+    uint64_t rsp_head; /* W */
+    uint64_t rsp_tail; /* R */
+    uint64_t reserved[3];
+};
+
+#define READ_CSR(field) CSR.Read64(offsetof(Csrs, field))
+#define WRITE_CSR(field, val) CSR.Write64(offsetof(Csrs, field), val)
+
+static inline void *VALID_ADDR_SIZE(uint32_t addr, uint32_t sz) {
+    CHECK(sz <= DRAM.size());
+    CHECK(addr < DRAM.size());
+    CHECK(addr <= DRAM.size() - sz);
+    return DRAM.at(addr);
+}
+
+static uint64_t real_index(uint64_t idx, uint64_t size)
+{
+    return idx & (size - 1);
+}
+
+static inline uint8_t htoi(char c)
+{
+    return c >= 'a' ? (c - 'a' + 10) : c - '0';
+}
+static void do_md5(const void *in, uint32_t size, uint8_t *out)
+{
+    FILE *infile = fopen("/tmp/.chaos", "wb"), *pf;
+    char hex[33];
+    int i;
+
+    CHECK(size == fwrite(in, 1, size, infile));
+    fclose(infile);
+    pf = popen("md5sum /tmp/.chaos", "r");
+    CHECK(fscanf(pf, "%s", hex) == 1);
+    pclose(pf);
+    for (i = 0; i < 16; i++)
+        out[i] = (htoi(hex[2 * i]) << 4) + htoi(hex[2 * i + 1]);
+}
+
+static int handle_cmd_request(struct chaos_mailbox_cmd *cmd)
+{
+    struct chaos_request *req;
+    void *in, *out;
+
+    // XXX: the implementation here is bad, kernel is able to hack with TOCTOU
+    CHECK(cmd->dma_size == sizeof(struct chaos_request));
+    req = (chaos_request*)VALID_ADDR_SIZE(cmd->dma_addr, cmd->dma_size);
+    in = VALID_ADDR_SIZE(req->input, req->in_size);
+    out = VALID_ADDR_SIZE(req->output, req->out_size);
+
+    switch (req->algo) {
+    case CHAOS_ALGO_ECHO:
+        memcpy(out, in, req->in_size);
+        return req->in_size;
+    case CHAOS_ALGO_MD5:
+        if (req->out_size < 0x10) {
+            // TODO: add logs?
+            return -EOVERFLOW;
+        }
+        do_md5(in, req->in_size, (uint8_t *)out);
+        return 0x10;
+    default:
+        CHECK(false);
+        return 0;
+    }
+}
+
+static int handle_cmd(struct chaos_mailbox_cmd *cmd)
+{
+    CHECK(cmd->code == CHAOS_CMD_CODE_REQUEST);
+    return handle_cmd_request(cmd);
+}
+
+static inline uint64_t queue_inc(uint64_t val, uint64_t size)
+{
+    return (++val) & ((size << 1) - 1);
+}
+
+static void push_rsp(struct chaos_mailbox_rsp *rsp)
+{
+    struct chaos_mailbox_rsp *queue = static_cast<chaos_mailbox_rsp*>(DRAM.at(READ_CSR(rspq_addr)));
+    const uint64_t rspq_size = READ_CSR(rspq_size);
+    uint64_t tail = READ_CSR(rsp_tail);
+
+    queue[real_index(tail, rspq_size)] = *rsp;
+    WRITE_CSR(rsp_tail, queue_inc(tail, rspq_size));
+}
+
+
+void HandleMailbox() {
+    const uint64_t cmdq_size = READ_CSR(cmdq_size);
+    uint64_t head = READ_CSR(cmd_head);
+    uint64_t tail = READ_CSR(cmd_tail);
+    struct chaos_mailbox_cmd *cmdq = static_cast<chaos_mailbox_cmd*>(DRAM.at(READ_CSR(cmdq_addr)));
+    struct chaos_mailbox_cmd *cmd;
+    struct chaos_mailbox_rsp rsp;
+
+    if (head == tail)
+        return;
+    cmd = &cmdq[real_index(head, cmdq_size)];
+    WRITE_CSR(cmd_head, queue_inc(head, cmdq_size));
+    rsp.retval = handle_cmd(cmd);
+    rsp.seq = cmd->seq;
+    push_rsp(&rsp);
+}
+
+} // namespace firmware
+
+namespace {
+
+void RunMain() {
+  constexpr int kEventFdFromHost = 5;
+  constexpr int kEventFdToHost = 6;
+  Event from(kEventFdFromHost), to(kEventFdToHost);
+
+  while (1) {
+    fprintf(stderr, "Waiting..\n");
+    from.WaitAndClear();
+    fprintf(stderr, "OAO!\n");
+    // TODO: fork, jmp to Code and wait for exit syscall instead of handling mailbox in parent
+    firmware::HandleMailbox();
+    to.Trigger();
+  }
+}
+
+} // namespace
 
 int main() {
-  alarm(60);
-  while(1);
+  RunMain();
   return 0;
 }
--- src/qemu/hw/misc/chaos/chaos.c
+++ src/qemu/hw/misc/chaos/chaos.c
@@ -7,6 +7,7 @@
 #include "qemu/osdep.h"
 
 #include <signal.h>
+#include <sys/eventfd.h>
 #include <sys/mman.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -42,6 +43,7 @@ typedef struct {
 
     struct share_mem csr, dram;
     pid_t devpid;
+    int evtfd_to_dev, evtfd_from_dev;
 } ChaosState;
 
 struct Csrs {
@@ -71,149 +73,20 @@ static void chaos_lower_irq(ChaosState *chaos)
     pci_set_irq(&chaos->pdev, 0);
 }
 
-// TODO
-#define USE_BUILTIN 1
-
-#if USE_BUILTIN
-
-static uint64_t chaos_csr_read(void *opaque, hwaddr addr, unsigned size);
-static void chaos_csr_write(void *opaque, hwaddr addr, uint64_t val, unsigned size);
-#define READ_CSR(chaos, field) chaos_csr_read(chaos, offsetof(struct Csrs, field), 8)
-#define WRITE_CSR(chaos, field, val) chaos_csr_write(chaos, offsetof(struct Csrs, field), val, 8)
-enum chaos_command_code {
-	CHAOS_CMD_CODE_REQUEST,
-};
-
-struct chaos_mailbox_cmd {
-    uint32_t seq;
-    enum chaos_command_code code;
-    uint32_t dma_addr;
-    uint32_t dma_size;
-};
-
-struct chaos_mailbox_rsp {
-    uint32_t seq;
-    uint32_t retval;
-};
-
-enum chaos_request_algo {
-	/* copy input to output, for testing purpose */
-	CHAOS_ALGO_ECHO,
-	CHAOS_ALGO_MD5,
-};
-
-struct chaos_request {
-	enum chaos_request_algo algo;
-	uint32_t input;
-	uint32_t in_size;
-	uint32_t output;
-	uint32_t out_size;
-};
-
-static inline void *VALID_ADDR_SIZE(ChaosState *chaos, uint32_t addr, uint32_t sz) {
-    g_assert(sz <= chaos->dram.size);
-    g_assert(addr < chaos->dram.size);
-    g_assert(addr <= chaos->dram.size - sz);
-    return chaos->dram.addr + addr;
-}
-
-static uint64_t real_index(uint64_t idx, uint64_t size)
-{
-    return idx & (size - 1);
-}
-
-static inline uint8_t htoi(char c)
-{
-    return c >= 'a' ? (c - 'a' + 10) : c - '0';
-}
-static void do_md5(const void *in, uint32_t size, uint8_t *out)
-{
-    FILE *infile = fopen("/tmp/.chaos", "wb"), *pf;
-    char hex[33];
-    int i;
-
-    g_assert(size == fwrite(in, 1, size, infile));
-    fclose(infile);
-    pf = popen("md5sum /tmp/.chaos", "r");
-    g_assert(fscanf(pf, "%s", hex) == 1);
-    pclose(pf);
-    for (i = 0; i < 16; i++)
-        out[i] = (htoi(hex[2 * i]) << 4) + htoi(hex[2 * i + 1]);
-}
-
-static int handle_cmd_request(ChaosState *chaos, struct chaos_mailbox_cmd *cmd)
-{
-    struct chaos_request *req;
-    void *in, *out;
-
-    // XXX: the implementation here is bad, kernel is able to hack with TOCTOU
-    g_assert(cmd->dma_size == sizeof(struct chaos_request));
-    req = VALID_ADDR_SIZE(chaos, cmd->dma_addr, cmd->dma_size);
-    in = VALID_ADDR_SIZE(chaos, req->input, req->in_size);
-    out = VALID_ADDR_SIZE(chaos, req->output, req->out_size);
-
-    switch (req->algo) {
-    case CHAOS_ALGO_ECHO:
-        memcpy(out, in, req->in_size);
-        return req->in_size;
-    case CHAOS_ALGO_MD5:
-        if (req->out_size < 0x10) {
-            // TODO: add logs?
-            return -EOVERFLOW;
-        }
-        do_md5(in, req->in_size, out);
-        return 0x10;
-    default:
-        g_assert(false);
-        return 0;
-    }
-}
-
-static int handle_cmd(ChaosState *chaos, struct chaos_mailbox_cmd *cmd)
-{
-    g_assert(cmd->code == CHAOS_CMD_CODE_REQUEST);
-    return handle_cmd_request(chaos, cmd);
-}
-
-static inline uint64_t queue_inc(uint64_t val, uint64_t size)
-{
-    return (++val) & ((size << 1) - 1);
-}
-
-static void push_rsp(ChaosState *chaos, struct chaos_mailbox_rsp *rsp)
-{
-    struct chaos_mailbox_rsp *queue = chaos->dram.addr + READ_CSR(chaos, rspq_addr);
-    const uint64_t rspq_size = READ_CSR(chaos, rspq_size);
-    uint64_t tail = READ_CSR(chaos, rsp_tail);
-
-    debug("queue[%ld] = {%u, %u}\n", real_index(tail, rspq_size), rsp->seq, rsp->retval);
-    queue[real_index(tail, rspq_size)] = *rsp;
-    WRITE_CSR(chaos, rsp_tail, queue_inc(tail, rspq_size));
-}
-
-#endif
-
 static void chaos_interrupt_to_device(ChaosState *chaos)
 {
-#if USE_BUILTIN
-    const uint64_t cmdq_size = READ_CSR(chaos, cmdq_size);
-    uint64_t head = READ_CSR(chaos, cmd_head);
-    uint64_t tail = READ_CSR(chaos, cmd_tail);
-    struct chaos_mailbox_cmd *cmdq = chaos->dram.addr + READ_CSR(chaos, cmdq_addr), *cmd;
-    struct chaos_mailbox_rsp rsp;
-
-    if (head == tail)
-        return;
-    cmd = &cmdq[real_index(head, cmdq_size)];
-    debug("head = %d, sz = %d, dma = 0x%x, dma_size = 0x%x\n", (int)head, (int)cmdq_size, cmd->dma_addr, cmd->dma_size);
-    WRITE_CSR(chaos, cmd_head, queue_inc(head, cmdq_size));
-    rsp.retval = handle_cmd(chaos, cmd);
-    rsp.seq = cmd->seq;
-    push_rsp(chaos, &rsp);
-    chaos_raise_irq(chaos);
-#else
-    // TODO: trigger eventfd to the external dev
-#endif
+    {
+        uint64_t one = 1;
+        ssize_t ret = write(chaos->evtfd_to_dev, &one, sizeof(one));
+        g_assert(ret == sizeof(one));
+    }
+    // TODO: move to thread
+    {
+        uint64_t dummy;
+        ssize_t ret = read(chaos->evtfd_from_dev, &dummy, sizeof(dummy));
+        g_assert(ret == sizeof(dummy));
+        chaos_raise_irq(chaos);
+    }
 }
 
 static void share_mem_init(const char *name, size_t size, struct share_mem *smem)
@@ -236,6 +109,12 @@ static void share_mem_exit(struct share_mem *smem)
     close(smem->fd);
 }
 
+static void dup_and_close(int oldfd, int newfd)
+{
+    dup2(oldfd, newfd);
+    close(oldfd);
+}
+
 static void launch_device(ChaosState *chaos)
 {
     // TODO: create a thread for polling eventfd
@@ -246,10 +125,10 @@ static void launch_device(ChaosState *chaos)
 #ifndef DEBUG
         close(2);
 #endif
-        dup2(chaos->csr.fd, 3);
-        dup2(chaos->dram.fd, 4);
-        close(chaos->csr.fd);
-        close(chaos->dram.fd);
+        dup_and_close(chaos->csr.fd, 3);
+        dup_and_close(chaos->dram.fd, 4);
+        dup_and_close(chaos->evtfd_to_dev, 5);
+        dup_and_close(chaos->evtfd_from_dev, 6);
         const char *const argv[] = { "entry", NULL };
         execv("/home/david942j/hitcon-2021-chaos/src/chaos/entry", (char *const *)argv);
         g_assert(false);
@@ -263,12 +142,14 @@ static void chaos_chip_init(ChaosState *chaos)
 {
     share_mem_init("dev-csr", sizeof(struct Csrs), &chaos->csr);
     share_mem_init("dev-dram", CHAOS_DEVICE_DRAM_SIZE, &chaos->dram);
+    chaos->evtfd_to_dev = eventfd(0, 0);
+    chaos->evtfd_from_dev = eventfd(0, 0);
     launch_device(chaos);
 }
 
 static void chaos_chip_exit(ChaosState *chaos)
 {
-    // XXX: this function is never called on QEMU ends..
+    // XXX: this function is not called on QEMU ends..
     kill(chaos->devpid, SIGKILL);
     wait(NULL);
     share_mem_exit(&chaos->dram);
